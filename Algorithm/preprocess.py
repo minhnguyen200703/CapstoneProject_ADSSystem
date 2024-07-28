1a8bbc9d211490a46f8cc07788d9dcd5552bbec2
an6122003 <an6122003>
2024-07-28 15:45:24 +0700

    Add preprocess distance func, and json

--- Algorithm/preprocess.py
+++ Algorithm/preprocess.py
@@ -1,8 +1,32 @@
+import json
+
 # Orders to Taskjobs
 
 
 
 #  Extracting distances for our simulated locations.json
+
+def filterDistanceFileAttributes(input_file, output_file):
+    with open(input_file, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    
+    filtered_data = []
+    for record in data['RECORDS']:
+        filtered_record = { # Eliminate other attributes
+            "DistanceKm": record["DistanceKm"],
+            "LocationFromName": record["LocationFromName"],
+            "LocationToName": record["LocationToName"]
+        }
+        filtered_data.append(filtered_record)
+    
+    with open(output_file, 'w') as f:
+        json.dump(filtered_data, f, indent=4)
+    print("Processed Distance Data Successfully, File Name:", output_file)
+
+# Run the func
+input_file = 'Data/Data_23042024/Distance.json'
+output_file = 'Data/distance.json'
+filterDistanceFileAttributes(input_file, output_file)
 
 
 
